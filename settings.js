7a571136eacd1d328690af923b23e781c04c530c
faith750 <165370756+faith750@users.noreply.github.com>
2025-02-05 17:54:56 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'B.E.L.I.E.V.E.R', // Your name
+  ownerNumber: '2348023483012', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
